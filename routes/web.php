a64491241cde64f35c87436e5b6a983669189899
Mutawali29 <qmutawali@gmail.com>
2026-08-14 11:25:25 +0700

    Add fallback route for routing diagnostics

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::fallback(function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'resolved_path' => $request->path(),
+        'full_url' => $request->fullUrl(),
+        'method' => $request->method(),
+    ], 200);
+});
