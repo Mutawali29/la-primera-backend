fc48f00bb7082b01230f43357cb7ac923ffe2220
Mutawali29 <qmutawali@gmail.com>
2026-08-14 11:17:24 +0700

    Add full SCRIPT_FILENAME/PHP_SELF override and inline debug mode

--- api/index.php
+++ api/index.php
@@ -1,5 +1,13 @@
 <?php
 
 $_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
+if (($_GET['__debug'] ?? null) === '1') {
+    header('Content-Type: application/json');
+    echo json_encode($_SERVER);
+    exit;
+}
 
 require __DIR__ . '/../public/index.php';
